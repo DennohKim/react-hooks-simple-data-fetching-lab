707369bcc87c71017ebcb454cefd6934b7153599
Dennis Kimathi <kimathidennis38@gmail.com>
2022-07-16 08:37:33 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+// import { server } from "../mocks/server";
+
+const API_URL = "https://dog.ceo/api/breeds/image/random"
+const App = () => {
+    const [ message, setMessage ] = useState(null);
+
+    useEffect(() => {
+        fetch(API_URL)
+        .then(res => res.json())
+        .then(data => setMessage(data.message))
+        
+    }, [])
+
+    if(!message){
+        return <p>Loading...</p>
+    }
+  return (
+    <div>
+        <h2>Here is a Dog</h2>
+        <img src={message} alt="A Random Dog" />
+    </div>
+  )
+}
+
+export default App;
